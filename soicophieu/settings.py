88fbed143529b8361914ad103bd0850a448ea88b
pmitung <pmitung@gmail.com>
2022-08-18 15:54:21 +0700

    deploy static files v2

--- soicophieu/settings.py
+++ soicophieu/settings.py
@@ -36,7 +36,7 @@ env.read_env()
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or 'django-insecure-glci_tg7i3#nm+b3-8*r_3rgsh#^492p@@*iilzq8w8@=-1@#l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "False")
 ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOST"), "127.0.0.1,localhost"]
 
 
