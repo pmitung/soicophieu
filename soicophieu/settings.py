9f64e2ad7e86d42a905e2b2439ae2567f5dad6d1
pmitung <pmitung@gmail.com>
2022-09-01 22:26:35 +0700

    update settings

--- soicophieu/settings.py
+++ soicophieu/settings.py
@@ -37,8 +37,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or 'django-insecure-glci_tg7i3#
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False) == 'True'
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOST")]
+# ALLOWED_HOSTS = ['*']
+ENV_ALLOWED_HOST = os.environ.get('DJANGO_ALLOWED_HOST') or None
+ALLOWED_HOSTS = []
+if ENV_ALLOWED_HOST is not None:
+    ALLOWED_HOSTS = [ ENV_ALLOWED_HOST ]
 
 # Application definition
 
